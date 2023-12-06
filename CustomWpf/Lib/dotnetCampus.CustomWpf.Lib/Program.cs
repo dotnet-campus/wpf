7380519a3085fa842ab541db30ad83fb5dacd9e7
lindexi <lindexi_gd@163.com>
2023-12-06 15:06:00 +0800

    尝试修复构建

--- CustomWpf/Lib/dotnetCampus.CustomWpf.Lib/Program.cs
+++ CustomWpf/Lib/dotnetCampus.CustomWpf.Lib/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Composition;
-using System.Windows.Navigation;
+// using System.Windows.Media;
+// using System.Windows.Media.Composition;
+// using System.Windows.Navigation;
 
 namespace dotnetCampus.WPF
 {
@@ -11,7 +11,7 @@ namespace dotnetCampus.WPF
     {
         static void Main(string[] args)
         {
-            Application application = new Application();
+            // Application application = new Application();
             //NavigationService applicationNavService = application.NavService;
         }
     }
